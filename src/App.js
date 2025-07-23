4a7b8995d15a1bc1b4d07092caadd9c9a7ad1a92
VedanshuGarg <140737181+VedanshuGarg@users.noreply.github.com>
2025-07-23 10:39:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MaintenancePage from "./pages/Maintenance";
 import Unauthorized from "./pages/Unauthorized";
